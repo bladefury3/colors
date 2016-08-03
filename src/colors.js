bb80ab5e6d2cc4e9accde88185c4a030592dfc75
Sidharath Chhatani <sid@ip-10-0-1-117.ec2.internal>
2016-08-03 11:54:49 -0400

    Added Sketch Download

--- src/colors.js
+++ src/colors.js
@@ -1,34 +1,50 @@
-$.getJSON("colors.json", function(json) {    
-    $('#iOS').html(iOSCode(json));
-    $('#android').html(androidCode(json));
-    $('#web').html(webCode(json));
+$(function() {
+	$.getJSON("colors.json", function(json) {    
+		$('#iOS').html(iOSCode(json));
+		$('#android').html(androidCode(json));
+		$('#web').html(webCode(json));
+
+		$('a#downloadSketch').click(function() {
+			var data = [];
+		    var sketch = '{"compatibleVersion":"1.0","pluginVersion":"1.1","colors":[';
+		    $.each(json, function(i, item) {				    	
+		    	color = '"#'+item+'"';
+	    	    data.push(color);
+		    });
+		    sketch += data.join();
+		    sketch += ']}';
+		    console.log(sketch);
+		    this.href = "data:text/plain;charset=UTF-8," + sketch;
+		});
+	});
+
+
+	function iOSCode(json){
+		var code = "";
+		$.each(json, function(i, item) {		
+			color = 'static let ' + i + ': UIColor = UIColor(hex: 0x' + item + ') <br>';
+			code += color;
+		});
+		return code;
+	}
+
+	function androidCode(json){
+		var androidBlock = '&lt;?xml version="1.0" encoding="utf-8"?&gt;<br>&lt;resources&gt;<br>';
+		$.each(json, function(i, item) {		
+			color = '&#9;&lt;color name="'+ i + '"&gt;#'+ item + '&lt;/color&gt;<br>';
+			androidBlock += color;
+		});
+		androidBlock += "&lt;/resources&gt;";
+		return androidBlock;
+	}
+
+	function webCode(json){
+		var web = '';
+		$.each(json, function(i, item) {		
+			webColor = '&#64'+ i + ': #'+ item + ';<br>';
+			web += webColor;
+		});	
+		return web;
+	}	
+
 });
-
-
-function iOSCode(json){
-	var code = "";
-	$.each(json, function(i, item) {		
-    color = 'static let ' + i + ': UIColor = UIColor(hex: 0x' + item + ') <br>';
-    code += color;
-  });
-	return code;
-}
-
-function androidCode(json){
-	var androidBlock = '&lt;?xml version="1.0" encoding="utf-8"?&gt;<br>&lt;resources&gt;<br>';
-	$.each(json, function(i, item) {		
-    color = '&#9;&lt;color name="'+ i + '"&gt;#'+ item + '&lt;/color&gt;<br>';
-    androidBlock += color;
-  });
-	androidBlock += "&lt;/resources&gt;";
-	return androidBlock;
-}
-
-function webCode(json){
-	var web = '';
-	$.each(json, function(i, item) {		
-    webColor = '&#64'+ i + ': #'+ item + ';<br>';
-    web += webColor;
-  });	
-	return web;
-}
