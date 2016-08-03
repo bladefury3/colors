4a2c94dda8c1f8af669577fbe9615aa6ccb83116
Sidharath Chhatani <sid@ip-10-0-1-117.ec2.internal>
2016-08-03 10:20:44 -0400

    First Commit

---
+++ example files/colors.m
@@ -0,0 +1,30 @@
+@objc class Colors: NSObject {
+    
+    // Brand Colors
+    static let FarmLogsGreen: UIColor = UIColor(hex: 0x41A259)
+    static let FarmLogsWhite: UIColor = UIColor(hex: 0xFFFFFF)
+    static let RainfallBlue: UIColor = UIColor(hex: 0x267FD9)
+    
+    // Semantic Colors
+    static let PrimaryText: UIColor = UIColor(hex: 0x454A54)
+    static let SecondaryText: UIColor = UIColor(hex: 0x8F96A4)
+    static let Disabled: UIColor = UIColor(hex: 0xC7CBD1)
+    static let Error: UIColor = UIColor(hex: 0xE06952)
+    static let Success: UIColor = UIColor(hex: 0x41A259)
+    static let Separator: UIColor = UIColor(hex: 0xDCDFE2)
+    static let SelectedCell: UIColor = UIColor(hex: 0xF9FAFA)
+    static let Background: UIColor = UIColor(hex: 0xFFF4F5)
+    
+}
+
+@objc class Fonts: NSObject {
+    
+}
+
+@objc class Borders: NSObject {
+    
+    static let CardCornerRadius: CGFloat = 3
+    static let ImageCornerRadius: CGFloat = 3
+    static let ButtonCornerRadius: CGFloat = 3
+    
+}
